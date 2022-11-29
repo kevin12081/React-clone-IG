c69c03250691dfebc65e3f75a721693bea33edfa
kevin12081 <kevin90298@gmail.com>
2022-11-29 14:46:45 +0800

    使用Redux, 接入限時動態欄API

--- src/pages/home/components/IG story/index.tsx
+++ src/pages/home/components/IG story/index.tsx
@@ -1,67 +1,26 @@
 import Item from "./Item";
+import { useGetIGStoiesQuery } from "services/homeService";
+import Loading from "components/Loading";
 
 const IGStory: React.FC = () => {
-  const data = [
-    {
-      id: 1,
-      name: "bruce_fe",
-      avatar: "/images/avatars/a1.png",
-    },
-    {
-      id: 2,
-      name: "max",
-      avatar: "/images/avatars/a2.png",
-    },
-    {
-      id: 3,
-      name: "fm",
-      avatar: "/images/avatars/a3.png",
-    },
-    {
-      id: 4,
-      name: "joanne",
-      avatar: "/images/avatars/a4.png",
-    },
-    {
-      id: 5,
-      name: "focus",
-      avatar: "/images/avatars/a5.png",
-    },
-    {
-      id: 6,
-      name: "louis",
-      avatar: "/images/avatars/a6.png",
-    },
-    {
-      id: 7,
-      name: "alvin",
-      avatar: "/images/avatars/a7.png",
-    },
-    {
-      id: 8,
-      name: "grace",
-      avatar: "/images/avatars/a8.png",
-    },
-    {
-      id: 9,
-      name: "rance",
-      avatar: "/images/avatars/a9.png",
-    },
-    {
-      id: 10,
-      name: "bruce_fe",
-      avatar: "/images/avatars/a10.png",
-    },
-  ];
+  const { data, isLoading } = useGetIGStoiesQuery("all");
+
+  // TODO del
+  console.log("data", data);
+  console.log("isLoading", isLoading);
 
   return (
     <div className="w-full h-[110px] box-border flex items-center overflow-x-auto overflow-y-hidden shadow-md no-scrollbar lg:my-8">
-      {/* <Item name="bruce_1234" avatar="/images/avatars/a1.png" />
-       */}
-      {data.map((item) => {
-        const { id, name, avatar } = item;
-        return <Item key={id} name={name} avatar={avatar} />;
-      })}
+      {isLoading && (
+        <div className="flex justify-center w-full">
+          <Loading />
+        </div>
+      )}
+      {!isLoading &&
+        data?.map((item) => {
+          const { id, name, avatar } = item;
+          return <Item key={id} name={name} avatar={avatar} />;
+        })}
     </div>
   );
 };
